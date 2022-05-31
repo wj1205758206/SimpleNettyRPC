f5cadb992a308ed86b80340cac45c75084c0e569
weijian <weijian@baidu.com>
2022-05-31 14:28:13 +0800

    init

---
+++ src/main/java/com/example/simplenettyrpc/SimpleNettyRpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplenettyrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNettyRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNettyRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplenettyrpc/SimpleNettyRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplenettyrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNettyRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
